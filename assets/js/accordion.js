15dc283d4de9651f1ee89c7c4a237178729e37af
Brent Thomas <brentmt24@gmail.com>
2015-07-27 08:27:42 -0500

    working (unsuccessfully) on "delete" key and acccordion menu

--- assets/js/accordion.js
+++ assets/js/accordion.js
@@ -9,27 +9,33 @@ var logoutEl = $('.logout');
 var subCloudEl = $('.subcloud');
 var subFilesEl = $('.subfiles');
 
-// menu1El.on('click', function() {
-//   subMenuEl.slideToggle(400);
-//   menu1El.toggleClass('active');
+// This makes all of them slide down
+// at the same time.
+
+menu1El.on('click', function() {
+  subMenuEl.slideToggle(400);
+  menu1El.toggleClass('active');
+});
+
+// For some reason the below set of functions
+// doesn't make "Cloud" slide down.
+
+// filesEl.on('click', function() {
+//   subFilesEl.slideToggle(400);
+//   filesEl.toggleClass('active');
 // });
 
-filesEl.on('click', function() {
-  subFilesEl.slideToggle(400);
-  filesEl.toggleClass('active');
-});
+// cloudEl.on('click', function() {
+//   subCloudEl.slideToggle(400);
+//   cloudEl.toggleClass('active');
+// });
 
-cloudEl.on('click', function() {
-  subCloudEl.slideToggle(400);
-  cloudEl.toggleClass('active');
-});
+// mailEl.on('click', function() {
+//   mailEl.toggleClass('active');
+// });
 
-mailEl.on('click', function() {
-  mailEl.toggleClass('active');
-});
-
-logoutEl.on('click', function() {
-  logoutEl.toggleClass('active');
-});
+// logoutEl.on('click', function() {
+//   logoutEl.toggleClass('active');
+// });
 
 })();
--- assets/js/keyboard.js
+++ assets/js/keyboard.js
@@ -5,6 +5,9 @@ var capsLockEl = $('.caps-lock');
 var letterKeysEl = $('.letter-keys');
 var capsEl = $('.caps');
 var spaceBarEl = $('.space-bar');
+var tabKeyEl = $('.tab-key');
+var returnKeyEl = $('.return');
+var deleteKeyEl = $('.delete-key');
 
 squareKey.on('click', function() {
   outputEl.append($(this).text());
@@ -14,11 +17,26 @@ spaceBarEl.on('click', function() {
   outputEl.append(' ');
 });
 
-capsLockEl.on('click', function() {
-  // alert('this function works!');
-
-  letterKeysEl.toggleClass('.caps');
-  capsEl.val($(this).val().toUpperCase());
+tabKeyEl.on('click', function() {
+  //this only gives one space instead of 5 :(
+  outputEl.append(' ' + ' ' + ' ' + ' ' + ' ');
 });
+
+returnKeyEl.on('click', function() {
+  outputEl.append('<br/>');
+});
+
+deleteKeyEl.on('click', function() {
+  // alert('this function works!');
+  var newOutput = outputEl.substring(0, outputEl.length - 1);
+  outputEl.val(newOutput);
+});
+
+// capsLockEl.on('click', function() {
+// alert('this function works!');
+
+// letterKeysEl.toggleClass('.caps');
+// letterKeysEl.html.;
+// });
 
 })();
